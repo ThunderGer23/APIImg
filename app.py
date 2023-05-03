6adc4e9d27940582f2808ee27647af8d04722880
ThunderGer <jaames11@hotmail.com>
2023-05-03 00:41:19 -0600

    Cambio de frase para notigram

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from routes.APIImg import Img
 from docs.docs import tags_metadata
 from os import environ as env
+from notigram import ping
+
+
+TOKEN='daa39d53-6283-47a1-b945-b7ee6528dde0'
+
+ping(TOKEN, 'Aquí APIImg Listo!')
 app = FastAPI(
     title= "Someone title :v/ vrgs",
     description= "Aquí una descripción chingona que nadie va leer nunca jámas a menos que esto falle o quieran una actualización >:'v, desgraciados flojos, lean la documentación!!, está hecha con mis lagrimas e.e",
--- routes/APIImg.py
+++ routes/APIImg.py
@@ -4,10 +4,14 @@ from helpers.Inter import Interprete
 from typing import List
 from threading import Thread
 import time
+from notigram import ping
+
+TOKEN='daa39d53-6283-47a1-b945-b7ee6528dde0'
 Img = APIRouter()
 
 @Img.post('/imgs')
 async def postImgsCompare(files: List[UploadFile] = File(...)):
+    ping(TOKEN, 'Inicio de Análisis para las imagenes!')
     start_time = time.time()
     filesave = []
     for file in files:
@@ -19,4 +23,5 @@ async def postImgsCompare(files: List[UploadFile] = File(...)):
     results = [h.start() or h.join() or {"similitud" :h.similarity, "porcentaje":h.percent, "imagenes a comparar":h.images} for h in [Thread(target=Interprete, args=(imgReference, f'./images/{i}')) for i in filesave]]
     end_time = time.time()
     print(end_time - start_time, "seconds")
+    ping(TOKEN, 'Análisis para las imagenes ok')
     return results
